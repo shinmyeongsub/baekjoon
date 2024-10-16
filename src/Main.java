f57768dadf2b3f5c96b79a5e2c7b767af07d4f3b
irm <aiden@irm.kr>
2024-10-16 14:59:33 +0900

    first commit

---
+++ src/Main.java
@@ -0,0 +1,10 @@
+import baekjoon.Bronze;
+
+public class Main {
+    public static void main(String[] args) {
+        Bronze bronze = new Bronze();
+//        bronze.q2557();
+//        bronze.q1000();
+        bronze.q1001();
+    }
+}
---
+++ src/baekjoon/Bronze.java
@@ -0,0 +1,24 @@
+package baekjoon;
+
+import java.util.Scanner;
+
+public class Bronze {
+
+    public void q2557() {
+        System.out.println("Hello World!");
+    }
+
+    public void q1000() {
+        Scanner scanner = new Scanner(System.in);
+        int A = scanner.nextInt();
+        int B = scanner.nextInt();
+        System.out.println(A+B);
+    }
+
+    public void q1001() {
+        Scanner scanner = new Scanner(System.in);
+        int A = scanner.nextInt();
+        int B = scanner.nextInt();
+        System.out.println(A-B);
+    }
+}
